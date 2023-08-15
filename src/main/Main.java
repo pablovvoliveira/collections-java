4610bf916b80b8bbecaf523a6828c5f289241ed0
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-14 21:26:21 -0300

    Resolvendo exercício de pesquisas com Map.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,10 @@ package main;
 import main.list.Ordenacao.OrdenarPessoas;
 import main.list.Pesquisa.CatalogoLivros;
 import main.list.operacoesBasicas.ListaTarefa;
+import main.map.Pesquisa.Estoque;
 import main.map.operacoesBasicas.AgendaContatos;
 import main.set.Ordenacao.CadastroProdutos;
+import main.set.Ordenacao.Produto;
 import main.set.Pesquisa.ListaTarefas;
 import main.set.Pesquisa.Tarefa;
 import main.set.operacoesBasicas.ConjuntoConvidados;
@@ -13,6 +15,7 @@ import main.set.operacoesBasicas.ConjuntoConvidados;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        /* Testando operações básicas com Map
         AgendaContatos agenda = new AgendaContatos();
 
         agenda.adicionarContato("Pablo", 99998888);
@@ -24,5 +27,23 @@ public class Main {
 
 
         agenda.exibirContatos();
+
+         */
+
+        // Testando pesquisas com Map
+        Estoque estoque = new Estoque();
+
+        estoque.adicionarProduto(1L, "Produto A", 1, 50.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 11.0);
+        estoque.adicionarProduto(3L, "Produto C", 3, 15.0);
+        estoque.adicionarProduto(8L, "Produto D", 2, 23.0);
+
+        System.out.println(estoque.obterProdutoMaisCaro());
+
+        System.out.println("Valor total do estoque: " + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+
+
+        estoque.exibirProdutos();
     }
 }
